ff29bad6c57b5b4de94214ef67b4de2f71c15e53
Suhanman <150568810+Suhanman@users.noreply.github.com>
2025-12-11 23:43:24 +0900

    Update StatsServiceApplication.java

--- stats-service/src/main/java/com/study/stats/StatsServiceApplication.java
+++ stats-service/src/main/java/com/study/stats/StatsServiceApplication.java
@@ -3,7 +3,14 @@ package com.study.stats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.study")
+@SpringBootApplication(
+        scanBasePackages = "com.study",
+        // [핵심] Redis 관련 자동 설정을 강제로 끕니다.
+        exclude = {
+                RedisAutoConfiguration.class,
+                RedisRepositoriesAutoConfiguration.class
+        }
+)
 public class StatsServiceApplication {
 
     public static void main(String[] args) {
