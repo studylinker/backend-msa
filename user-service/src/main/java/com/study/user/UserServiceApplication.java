90aaafcbe49b22563505b74e1b0a1fb9c2e74f20
Suhanman <150568810+Suhanman@users.noreply.github.com>
2025-12-12 00:19:48 +0900

    Update UserServiceApplication.java

--- user-service/src/main/java/com/study/user/UserServiceApplication.java
+++ user-service/src/main/java/com/study/user/UserServiceApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
+        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+    })
 @ComponentScan(basePackages = {
         "com.study.user",             // user-service 자체
         "com.study.common.security",  // common-security module
         "com.study.common.web"        // 공통 예외 처리 모듈
-},excludeName = {
-        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
-        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
-    })
+})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
