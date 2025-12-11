cb430d5f680a4907732bd42d3245c336487f9b31
Suhanman <150568810+Suhanman@users.noreply.github.com>
2025-12-12 00:14:28 +0900

    Update RecommendServiceApplication.java

--- recommend-service/src/main/java/com/study/recommend/RecommendServiceApplication.java
+++ recommend-service/src/main/java/com/study/recommend/RecommendServiceApplication.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.study.recommend",      // 현재 recommendation-service
                 "com.study.common.security", // JwtTokenProvider, Filter
                 "com.study.common.web"       // WebConfig 
-        }
+        },excludeName = {
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
+        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+    }
 )
 public class RecommendServiceApplication {
     public static void main(String[] args) {
