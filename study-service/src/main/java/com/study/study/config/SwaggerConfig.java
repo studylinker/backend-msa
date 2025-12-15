01a5978b4e6bd9c87d1ccc89e4c8b8bc6fefc9c2
KHS <166093828+HSKIM2024@users.noreply.github.com>
2025-12-15 12:05:03 +0900

    Add files via upload

---
+++ study-service/src/main/java/com/study/study/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.study.study.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Study Service API")
+                        .description("스터디 서비스 API - 스터디 그룹, 일정, 게시물, 출석 관리")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
